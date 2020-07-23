a8c94d06af1c1500ce02b3d728e8fdb8512c0d3e
Victor Hugo <victor_nascimento1996@hotmail.com>
2020-07-23 03:20:36 -0300

    Adicionando comentarios

--- src/main/java/com/unisys/unisys/repository/JobRepository.java
+++ src/main/java/com/unisys/unisys/repository/JobRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.unisys.unisys.models.Job;
 
-
+//Interface criada para utilizar metodos prontos para realizar persistências.
 
 public interface JobRepository extends JpaRepository<Job, Integer>{
 
--- src/main/java/com/unisys/unisys/repository/TaskRepository.java
+++ src/main/java/com/unisys/unisys/repository/TaskRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.unisys.unisys.models.Task;
 
-
+//Interface criada para utilizar metodos prontos para realizar persistências.
 public interface TaskRepository extends JpaRepository<Task, Integer>{
 
 	Task findById(int id);	
